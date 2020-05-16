faffd4042a926cf5ed923c4acbd07b5c1c1bea46
David Darnes <me@daviddarnes.com>
2020-05-16 20:06:18 +0100

    More clearing up, add caching to images, add lock files for speed

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const fetch = require("node-fetch");
 const url = require("url");
+const chalk = require('chalk');
 const ghostContentAPI = require("@tryghost/content-api");
 
 // Get posts using Ghost Content API
@@ -30,23 +31,17 @@ const getPages = async (api, failPlugin) => {
   }
 };
 
-// Get all images
-const downloadImage = async (inputURI, outputPath, cache, failPlugin) => {
+// Download images
+const downloadImage = async (inputURI, outputPath, failPlugin) => {
   try {
-    // If file is in cache restore it
-    if (await cache.has(outputPath)) {
-      console.log("Restoring from cache: " + outputPath);
-      await cache.restore(outputPath);
-    } else {
-      // Grab file data from remote inputURI
-      const res = await fetch(inputURI);
-      const fileData = await res.buffer();
 
-      // Write the file and cache it
-      await fs.outputFile(outputPath, fileData);
-      console.log("Caching: " + outputPath);
-      await cache.save(outputPath);
-    }
+    // Grab file data from remote inputURI
+    const res = await fetch(inputURI);
+    const fileData = await res.buffer();
+
+    // Write the file and cache it
+    await fs.outputFile(outputPath, fileData);
+
   } catch (error) {
     failPlugin("Image file error", { error });
   }
@@ -122,6 +117,7 @@ module.exports = {
       cache
     }
   }) => {
+
     // Ghost images path
     const ghostImagePath = ghostURL + "/content/images/";
 
@@ -140,10 +136,13 @@ module.exports = {
 
     // Find all images
     const findImages = (allContent) => {
+
+      // Find all posts and pages with images in the HTML
       const htmlWithImages = allContent
         .filter((item) => item.html && item.html.includes(ghostImagePath))
         .map((item) => item.html);
 
+      // Get all images from posts and pages
       const htmlImages = htmlWithImages
         .map((html) =>
           html.split('"').filter((slice) => {
@@ -152,6 +151,7 @@ module.exports = {
         )
         .flat();
 
+      // Get all feature images from posts and pages
       const featureImages = allContent
         .filter(
           (item) =>
@@ -159,6 +159,7 @@ module.exports = {
         )
         .map((item) => item.feature_image);
 
+      // Clear up and possible duplicates
       const allImages = [...new Set([...htmlImages, ...featureImages])];
 
       return allImages;
@@ -166,35 +167,58 @@ module.exports = {
 
     // Generate all images, posts and pages…
     await Promise.all([
+
       // Replace Ghost image paths with local ones
-      ...findImages([...posts, ...pages]).map((image) => {
+      ...findImages([...posts, ...pages]).map( async (image) => {
+
+        // Image destination
         const dest = image.replace(ghostImagePath, assetsDir);
-        downloadImage(image, dest, cache, failPlugin);
+
+        // Check if image is in Netlify cache
+        if (await cache.has(dest)) {
+
+          // Restore image from cache
+          await cache.restore(dest);
+          console.log(chalk.green('Restored from cache: ') + chalk.green.underline(dest));
+
+        } else {
+
+          // …otherwise download the image and cache it
+          await downloadImage(image, dest, failPlugin);
+          await cache.save(dest);
+          console.log(chalk.cyan('Downloading and caching: ') + chalk.cyan.underline(dest));
+        }
       }),
 
       // Generate markdown posts
       ...posts.map(async (post) => {
-        console.log("Creating post: " + post.title);
+
+        // Prefix filename with date, Jekyll style formatting
         const filename = postDatePrefix
           ? `${post.published_at.slice(0, 10)}-${post.slug}`
           : post.slug;
+
+        // Create post markdown file with content
         await writeMarkdown(
           postsDir,
           `${filename}.md`,
           mdTemplate(post, ghostImagePath, assetsDir, postsLayout),
           failPlugin
         );
+        console.log(chalk.gray('Generated post: ') + chalk.gray.underline(post.title));
       }),
 
       // Generate markdown pages
       ...pages.map(async (page) => {
-        console.log("Creating page: " + page.title);
+
+        // Create page markdown file with content
         await writeMarkdown(
           pagesDir,
           `${pagesDir + page.slug}.md`,
           mdTemplate(page, ghostURL, ghostImagePath, assetsDir, pagesLayout),
           failPlugin
         );
+        console.log(chalk.gray('Generated page: ') + chalk.gray.underline(page.title));
       })
     ]);
   }
